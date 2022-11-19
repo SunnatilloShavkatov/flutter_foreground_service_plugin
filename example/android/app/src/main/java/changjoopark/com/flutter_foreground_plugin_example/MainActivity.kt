6e02c8e1384fb7c003e02b2045ca158fca34b389
Sunnatillo <sunnatilloshavkatov@gmail.com>
2022-11-19 15:55:33 +0500

    gradle update , build done

---
+++ example/android/app/src/main/java/changjoopark/com/flutter_foreground_plugin_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package changjoopark.com.flutter_foreground_plugin_example
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
